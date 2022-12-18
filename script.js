dcdb2f4b72c77127bbae06769ff9846fc3007d07
Ankit <108215167+Kushhhhh008@users.noreply.github.com>
2022-12-18 15:48:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function weather(city){
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=10a53144c812dee0c86d6ccc2c862a7b", requestOptions)
+    .then(response => response.text())
+    .then(result => displayWeather(result))
+    .catch(error => console.log('error', error));
+}
+function displayWeather(result) 
+{
+ const data = JSON.parse(result)
+ const { name } = data;
+ const { icon, description } = data.weather[0];
+ const { temp, humidity } = data.main;
+ const { speed } = data.wind;
+ console.log(name,icon,description,temp,humidity,speed);
+document.querySelector(".city").innerText = "Weather in " + name;
+document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon + ".png"
+document.querySelector(".description").innerText = description;
+document.querySelector(".temp").innerText = temp + "°C";
+document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+document.querySelector(".wind").innerText = "Wind speed: " + speed + "km/h";
+document.querySelector(".weather").classList.remove("loading");
+document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1920x1080/?wallpaper,"+ name +"')"
+}
+
+function search() {
+    weather(document.querySelector(".search-bar").value);
+}
+document
+.querySelector(".search button")
+.addEventListener("click", function() {
+    search();
+
+    document.querySelector(".search-bar").addEventListener("keyup", function(event) {
+        if (event.key == "Enter") {
+            weather.search
+        }
+    })
+});
+
+weather("Delhi");
